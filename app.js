dadf5965832481294104204de151c17c8615f6d5
Salvatore-Fiannaca <salvatore.fiannaca1@gmail.com>
2020-08-07 15:00:15 +0200

    up

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 //  Node.js     (localhost:3000)
 const path = require('path')
 const express = require('express')
-const hbs = require('hbs')
+const ejs = require('ejs')
 
 const app = express()
 
-// Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
-
 // Setup handlebars engine and views location
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 
-// Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
     res.render('index', {
