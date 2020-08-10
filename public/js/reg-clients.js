e45473d2e40b6273066a280256d935a0ad52c23d
JiL <salvatore.fiannaca1@gmail.com>
2020-08-11 00:13:58 +0200

    up

--- public/js/reg-clients.js
+++ public/js/reg-clients.js
@@ -1,1 +1,31 @@
-console.log("Js client-side is load");
+
+console.log('Client side javascript file is loaded!')
+
+const clientForm = document.querySelector('#clientForm')
+
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+clientForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const firstName = document.querySelector('#firstName').value
+    const lastName = document.querySelector('#lastName').value
+
+    messageOne.textContent  = 'Loading...'
+    messageTwo.textContent = ''
+    
+    fetch('http://localhost:3000/tasks').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+          messageOne.textContent = data.error
+        } else {
+            messageOne.textContent = "Cliente registrato correttamente"
+          //messageOne.textContent = data.body
+          //messageTwo.textContent = data
+        }
+        }
+    )}
+    )
+  
+})
