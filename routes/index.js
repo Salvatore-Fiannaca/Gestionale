fa81305f269a53a6f1c51c9a52304adf43ee25bf
Salvatore-Fiannaca <salvatore.fiannaca1@gmail.com>
2020-08-16 19:01:43 +0200

    fix user permissions for show clients

--- routes/index.js
+++ routes/index.js
@@ -54,7 +54,7 @@ const auth = require('../config/auth')
  /**
  * -------------- GET ROUTES ----------------
  */
-
+//  LOGIN / DASHBOARD
 router.get('/', (req, res) => {
     if (req.isAuthenticated()) {
         res.render('pages/index');
@@ -80,32 +80,22 @@ router.get('/client', auth, (req, res) => {
         res.render('pages/clients');
 })
 
-router.get('/clients/:id', auth, async (req, res) => {
-    //const _id = req.params.id
-    try {
-        const client = await Client.findOne({_id: "5f38f01c35619b3d1d091641"})
-    } catch (e) {
-        console.log(e)
-    }
-    res.render('pages/show-clients');
-})
-
 router.get('/clients', auth, async (req, res) => {
-    //Manual setting owner
-    owner = "5f38f01c35619b3d1d091641"
+    owner = req.session.passport.user
     const filter = await Client.find({owner: owner})
     res.render('pages/show-clients', {clientList: filter});
 })
-
+//--------UNDER CONSTRUCTION------------
 router.get('/practice', auth, (req, res) => {
     res.render('pages/practice')
 })
 
 router.get('/practices', auth, async (req, res) => {
-    const clientList = await Client.find()
+    owner = req.session.passport.user
+    const clientList = await Client.find({owner: owner})
     res.render('pages/show-practices', {clientList: clientList});
 })
-
+//--------------------------------------
 router.get('/404', (req, res) => {
     res.render('pages/404')
 })
