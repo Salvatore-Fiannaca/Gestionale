6d73d10de27e2c696afdfccec574591a1c49e227
JiL <salvatore.fiannaca1@gmail.com>
2020-08-20 08:44:16 +0200

    multer fileFilter

--- config/multer.js
+++ config/multer.js
@@ -1,10 +1,7 @@
 const multer = require('multer')
 
 const storage = multer.diskStorage({
-    destination: function(req, file, callback) {
-        //const dir = process.env.HOME + '/Desktop/Gestionale/upload/clients'
-        callback(null, 'upload/clients')
-    },
+    destination: 'upload/client',
     filename: function(req, file, callback) {
         //const parts = file.mimetype.split("/");
         //callback(null, `${file.fieldname}-${Date.now()}.${parts[1]}`)
@@ -12,6 +9,25 @@ const storage = multer.diskStorage({
     }
 })
 
-const upload = multer({storage: storage}).array('files', 12)
+const upload = multer({
+    storage: storage,
+    fileFilter(req, file, callback) {
+        if (file.originalname.match(/\.(doc|docx)$/)) {
+            //return callback(new Error ('Please upload a Word document'))
+            console.log("You are upload a Word document");
+        }
+        if (file.originalname.match(/\.(pdf|jpg|jpeg|png)$/)) {
+            //return callback(new Error ('Please upload a Word document'))
+            console.log("You are upload an Image");
+
+        }
+
+        callback(null, true)
+    },
+    limits: {
+        fileSize: 1024 * 1024
+    }
+
+}).array('files', 12)
 
 module.exports = upload
