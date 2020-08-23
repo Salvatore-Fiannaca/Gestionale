942fa7413a4bbdc8164ba56ed90c3f5d54f070d4
Salvatore-Fiannaca <salvatore.fiannaca1@gmail.com>
2020-08-23 09:11:05 +0200

    up

--- routes/client.js
+++ routes/client.js
@@ -29,7 +29,38 @@ router.post('/client', auth, async (req, res) => {
          console.log(e)
     }
 
-  })
+})
+
+//      WORKING ON UPDATE CLIENT
+router.post('/update_:id', auth, async (req, res) => {
+    const id = req.params.id
+    //const code = req.body.fiscalCode
+    const client = Client.findOne({_id: id, owner: req.user._id})
+    console.log(client);
+})
+    /*  
+    const newClient = await new Client({
+        "profile.firstName": req.body.firstName,
+        "profile.lastName": req.body.lastName,
+        "profile.fiscalCode": code,
+        "address.street": req.body.address,
+        "address.city": req.body.city,
+        "address.state": req.body.state,
+        "address.zipCode": req.body.zipCode,
+        "contacts.email": req.body.email,
+        "contacts.phone": req.body.phone,
+        ...req.body,
+        owner: req.user._id,
+    })
+    try {
+        await newClient.save()
+        res.redirect(`/upload_${code}`)
+        } catch (e) {
+         console.log(e)
+        
+        }
+    }) 
+    */
 
 /**
  * -------------- GET ROUTES ----------------
@@ -42,13 +73,13 @@ router.get('/client', auth, (req, res) => {
 router.get('/edit-client_:id', auth, async (req, res) => {
     owner = req.session.passport.user
     const client = await Client.find({owner: owner, _id: req.params.id})
-    console.log(client);
     res.render('pages/edit-client', {clientList: client, n: 1});
 })
 
 router.get('/clients', auth, async (req, res) => {
     owner = req.session.passport.user
     const filter = await Client.find({owner: owner})
+    console.log(filter);
     res.render('pages/show-clients', {clientList: filter, n: 1});
 })
 
